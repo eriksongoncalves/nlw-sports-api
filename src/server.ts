95137f72b3aadbba55aa4f8012920777c3d34715
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-09-15 22:25:15 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 import { convertHourToMinutes } from './utils/convert-hour-to-minutes'
@@ -11,6 +12,7 @@ const app = express()
 const prisma = new PrismaClient()
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/games', async (req, res) => {
   const games = await prisma.game.findMany({
